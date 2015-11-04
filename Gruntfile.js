502fe3d577dc9dc7b160b38d87d16fb985dc04ea
Jeremy Othieno <j.othieno@gmail.com>
2015-11-04 14:58:13 +0100

    Add Grunt configurations to generate asset bundles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,63 @@
 module.exports = function(grunt){
     grunt.initConfig({
         pkg:grunt.file.readJSON("bower.json"),
+        dir:{
+            css:"assets/css",
+            js:"assets/js",
+            bundles:"assets/bundles",
+            vendors:"vendor"
+        },
+        concat:{
+            options:{
+                separator:";"
+            },
+            bundles:{
+                files:{
+                    "<%= dir.bundles %>/asset.bundle.core.css":[
+                        "<%= dir.css %>/*.css",
+                        "!<%= dir.css %>/geotagx-ol.css"
+                    ],
+                    "<%= dir.bundles %>/asset.bundle.core.js":[
+                        "<%= dir.js %>/*.js",
+                        "!<%= dir.js %>/geotagx-ol.js"
+                    ],
+                    "<%= dir.bundles %>/asset.bundle.geolocation.css":[
+                        "<%= dir.css %>/geotagx-ol.css"
+                    ],
+                    "<%= dir.bundles %>/asset.bundle.geolocation.js":[
+                        "<%= dir.js %>/geotagx-ol.js"
+                    ]
+                }
+            },
+            vendors:{
+                files:{
+                    "<%= dir.bundles %>/asset.bundle.geolocation.css":[
+                        "<%= dir.vendors %>/openlayers/ol.css",
+                        "<%= dir.bundles %>/asset.bundle.geolocation.css"
+                    ],
+                    "<%= dir.bundles %>/asset.bundle.geolocation.js":[
+                        "<%= dir.vendors %>/openlayers/ol.js",
+                        "<%= dir.bundles %>/asset.bundle.geolocation.js"
+                    ]
+                }
+            }
+        },
+        cssmin:{
+            bundles:{
+                files:[{
+                    expand:true,
+                    src:"<%= dir.bundles %>/*.css"
+                }]
+            }
+        },
+        uglify:{
+            bundles:{
+                files:[{
+                    expand:true,
+                    src:"<%= dir.bundles %>/*.js"
+                }]
+            }
+        },
         jshint:{
             files:[
                 "Gruntfile.js",
@@ -19,9 +76,13 @@ module.exports = function(grunt){
             files:["test/**/*.html"]
         }
     });
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-qunit");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
     grunt.registerTask("test",    ["jshint","qunit"]);
-    grunt.registerTask("default", ["jshint","qunit"]);
+    grunt.registerTask("bundle",  ["concat:bundles","cssmin","uglify","concat:vendors"]);
+    grunt.registerTask("default", ["test","bundle"]);
 };
